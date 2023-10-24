c940432cc1e8c355d236473a0fb0e245f88ac5ae
nuzulul <nuzulz@gmail.com>
2023-10-24 11:02:16 +0700

    try fix : increase click timeout waitforfilechoser

--- instagram/Client.js
+++ instagram/Client.js
@@ -465,6 +465,7 @@ class Client extends ClientEvent {
             }
 
             console.log("membuka tab baru")
+            
             const currentPage = await this.openNewPage();
 
             await currentPage.goto(URLS.BASE, {
@@ -475,15 +476,17 @@ class Client extends ClientEvent {
             try{
               await currentPage.waitForSelector("svg[aria-label='New post']")
             } catch(e) {
-              reject(e)
+              return reject(e)
             }
             console.log("siap upload")
             
-            await currentPage.waitForTimeout(3000);
+            await currentPage.waitForTimeout(5000);
 
             try{
               await currentPage.evaluate(Injects);
-            }catch{}
+            }catch(e){
+              return reject(e)
+            }
 
             const openNewPostModal = await currentPage.evaluate(_ => {
                 return window.IGJS.openNewPostModal();
@@ -498,12 +501,24 @@ class Client extends ClientEvent {
                 }
                 return reject("Post modal not found. This is an error, please make a report to us.");
             };
+            
+            try{
+              await currentPage.waitForFunction(
+                '[...document.querySelectorAll("button")].find(b => b.innerText.toLowerCase().match("select from computer"))',
+              );
+              console.log('found select from computer')
+            }catch{
+              console.log('not found select from computer')
+              return reject("Post modal not found.");
+            }            
 
             await Promise.all([
                 currentPage.waitForFileChooser({timeout: 120000}).then(fileChooser => {
+                    console.log('waitForFileChooser')
                     return fileChooser.accept(media.map(x => x.path));
                 }),
-                currentPage.waitForTimeout(1000).then(() => {
+                currentPage.waitForTimeout(5000).then(() => {
+                    console.log('click select from computer')
                     return currentPage.evaluate(() => {
                         return window.IGJS.clickSelectFromComputerButton()
                     })
